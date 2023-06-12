7bd166cb40a2c13332b46723528195adde66dee6
Karim Oudrhiri <k.oudrhiri86@gmail.com>
2023-06-12 15:46:36 +0100

    styles, sort by search

--- components/SearchForm/index.js
+++ components/SearchForm/index.js
@@ -3,6 +3,8 @@ import { employees } from "@/test_daten/test_daten";
 
 const EmployeeProfile = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [sortOrder, setSortOrder] = useState("default"); // Zustandsvariable für Sortierreihenfolge
+
   const searchResults = employees.filter((employee) =>
     employee.name.toLowerCase().includes(searchValue.toLowerCase())
   );
@@ -25,18 +27,56 @@ const EmployeeProfile = () => {
     setSearchValue("");
   }
 
+  function handleSort(event) {
+    const sort = event.target.value;
+    setSortOrder(sort);
+  }
+
+  // Mitarbeiterliste basierend auf Sortierreihenfolge sortieren
+  let sortedEmployees = [...(searchValue ? searchResults : employees)];
+
+  if (sortOrder === "name") {
+    sortedEmployees.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortOrder === "position") {
+    sortedEmployees.sort((a, b) => a.position.localeCompare(b.position));
+  } else if (sortOrder === "age") {
+    sortedEmployees.sort((a, b) => a.age - b.age);
+  } else if (sortOrder === "startDate") {
+    sortedEmployees.sort(
+      (a, b) => new Date(a.startDate) - new Date(b.startDate)
+    );
+  }
+
   return (
     <>
       <h2>Mitarbeiterliste</h2>
       <form onSubmit={handleSearch}>
-        <label htmlFor="search_name">Suche:</label>
-        <input type="text" name="search_name" id="search_name" />
-        <button type="submit">Suchen</button>
+        <div className="search-bar">
+          <input type="text" name="search_name" id="search_name" />
+          <button type="submit" className="search-button">
+            Suchen
+          </button>
+          <button
+            type="button"
+            onClick={handleShowAll}
+            className="show-all-button"
+          >
+            Alle anzeigen
+          </button>
+        </div>
       </form>
 
-      <button onClick={handleShowAll}>Alle anzeigen</button>
+      {/* Sortier-Dropdown-Menü */}
+      <label htmlFor="sort_order">Sortieren nach:</label>
+      <select id="sort_order" name="sort_order" onChange={handleSort}>
+        <option value="name">Name</option>
+        <option value="age">Alter</option>
+        <option value="startDate">Startdatum</option>
+        <option value="position">Position</option>
+      </select>
+
       <ul>
-        {(searchValue ? searchResults : employees).map((employee) => (
+        {sortedEmployees.map((employee) => (
           <li key={employee.id}>
             <div>
               <img
--- styles.js
+++ styles.js
@@ -10,5 +10,32 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
+    background-color: black;
+  }
+
+  h2 {
+    color: gold;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+
+  img {
+    border: 2px solid gold;
+    cursor: pointer;
+  }
+
+  div {
+    color: gold;
+  }
+
+  p {
+    color: gold;
   }
 `;
